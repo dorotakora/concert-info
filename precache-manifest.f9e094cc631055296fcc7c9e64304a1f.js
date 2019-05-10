097d6b0791fc53b89f067794325aee2d233a3d33
dorotakora <dorota.kora@o2.pl>
2019-05-10 18:10:37 +0200

    Updates

---
+++ precache-manifest.f9e094cc631055296fcc7c9e64304a1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ccb640fdce96ab15858252af4e291c",
+    "url": "/todo_react_app/index.html"
+  },
+  {
+    "revision": "03a4513dc66decc71867",
+    "url": "/todo_react_app/static/css/main.581cbc96.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/todo_react_app/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "03a4513dc66decc71867",
+    "url": "/todo_react_app/static/js/main.ac34f6cd.chunk.js"
+  },
+  {
+    "revision": "0583c8e0969b63e13c75",
+    "url": "/todo_react_app/static/js/runtime~main.c26e3788.js"
+  }
+]);
